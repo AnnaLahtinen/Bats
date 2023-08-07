6f4e9f02f60ab86a954e163727d453f5aff2be24
Aakash <8xaakash@gmail.com>
2023-08-07 17:41:29 +0530

    Apply animation to logo
    applied the animation to the logo when you click on it it'll play the animation and bat sounds alternatively

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ bars.addEventListener("click", () => {
         links.style.display = "flex";
     }
 });
+
+                /* Logo animation */
+
+const logo = document.getElementById("batLogo");
+logo.addEventListener("click", () => {
+    if (logo.className === "logo-img") {
+        logo.className = "logo-img animation";
+    }
+    else {
+        logo.className = "logo-img";
+        batSounds = document.getElementById("batSounds");
+        batSounds.play();
+    }
+    
+});
