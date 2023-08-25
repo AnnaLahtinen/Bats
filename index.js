68b3efab4ecb95254060462f26d31d64cd2eb760
aakashbishnoi1 <8xaakash@gmail.com>
2023-08-25 15:46:40 +0530

    Add search feature

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ let welcomeTxt = "to the bat world"; /* The text */
 let speed = 200; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
-  if (i < welcomeTxt.length) { 
-   const welcome =  document.getElementById("welcome")
-   welcome.innerHTML += welcomeTxt.charAt(i);
-    i++;
-  setTimeout(typeWriter, speed);
+    if (i < welcomeTxt.length) { 
+        const welcome =  document.getElementById("welcome")
+        welcome.innerHTML += welcomeTxt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
 
-}
+    }
 }
 
 
